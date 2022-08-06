9df4280aaa4d49bf38c475d7517f0d153124f2cd
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-06 18:44:19 +0400

    added effect on button clicks.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,13 +6,17 @@ import UIKit
 class ViewController: UIViewController {
     var player: AVAudioPlayer!
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha=0.5
         playSound(soundName: sender.currentTitle!)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha=1
+        
+        }
     }
     
     func playSound(soundName: String) {
